fdd1c8d87427a8569f24dd10d983207998e941d8
Clément Renaud <clement.renaud@gmail.com>
2015-12-15 10:56:56 +0100

    add header.js #33

---
+++ client/partials/header.js
@@ -0,0 +1,8 @@
+Template.header.rendered = function() {
+  // sidebar
+  $('.collapsing').sideNav({
+    closeOnClick: true,
+    menuWidth: 300, // Default is 240
+    edge: 'left' // Choose the horizontal origin
+  });
+}
