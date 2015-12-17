b7f47e7e8538b32a646e45d21b0404dd9c406bae
Clément Renaud <clement.renaud@gmail.com>
2015-12-17 22:08:26 +0100

    clean edit mode

--- client/networkTools/editMode/editMode.js
+++ client/networkTools/editMode/editMode.js
@@ -5,33 +5,17 @@ Template.editMode.rendered = function() {
 
 Template.editMode.events = {
 
-        'click .toggle-node-edit-mode': function(e,template) {
+        'change #toggle-edge-edit-mode': function(e,template) {
             var net = template.view.parentView.parentView._templateInstance.network.get().net;
-            nodeEditMode = !nodeEditMode
-            console.log("nodeEditMode", nodeEditMode)
-            if (!nodeEditMode){
+            var edgeEditMode = e.target.checked;
+
+            if (!edgeEditMode){
                 net.edgehandles("disable");
-                $(e.target).text("node edit").toggleClass("btn-flat")
             }
             else {
                 net.edgehandles("enable")
-                $(e.target).text("node edit on").toggleClass("btn-flat")
             }
             return;
 
-        },
-        'click .toggle-edge-edit-mode': function(e,template) {
-            var net = template.view.parentView.parentView._templateInstance.network.get().net;
-            edgeEditMode = !edgeEditMode
-            console.log("edgeEditMode", edgeEditMode)
-            if (!edgeEditMode){
-                net.edgehandles("disable");
-                $(e.target).text("edge edit").toggleClass("btn-flat")
-            }
-            else {
-                net.edgehandles("enable")
-                $(e.target).text("edge edit on").toggleClass("btn-flat")
-            }
-            return;
         }
 }
