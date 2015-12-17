a0fdb0f5b1bc7ad811de9316ea12a670c9bab2a4
Clément Renaud <clement.renaud@gmail.com>
2015-12-17 18:37:39 +0100

    move algos to algobox

--- client/boxes/titlebox/titlebox.js
+++ client/boxes/titlebox/titlebox.js
@@ -1,5 +1,8 @@
 Template.titlebox.events =  {
   'click #toggle-toolbox' : function() {
     $('#toolbox').toggle();
+  },
+  'click #toggle-algobox' : function() {
+    $('#algobox').toggle();
   }
 }
