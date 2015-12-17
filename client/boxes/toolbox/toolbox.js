f0e690feb1e4005d451db7743e85dbc0200fc970
ungentilgarçon <gregory.bahde@laposte.net>
2015-12-17 15:09:31 +0100

    implemented Advanced Algorithms list

--- client/boxes/toolbox/toolbox.js
+++ client/boxes/toolbox/toolbox.js
@@ -91,94 +91,94 @@ function createLinearScale(domain, range) {
 }
 
 
-// var scale = d3.scale.linear()
-//                     .domain([100, 500])
-//                     .range([10, 350]);
+// // var scale = d3.scale.linear()
+// //                     .domain([100, 500])
+// //                     .range([10, 350]);
 
-//color mean
-function colorMean(color1, color2) {
-    console.log(color1, color2);
-    rgb = hexToRgb(color1)
-    shc = hexToRgb(color2)
-        console.log("rgb", rgb)
-        console.log("shc", shc)
-    var r = rgb[Object.keys(rgb)[0]];
-    var g = rgb[Object.keys(rgb)[1]];
-    var b = rgb[Object.keys(rgb)[2]];
-    var s = shc[Object.keys(shc)[0]];
-    var h = shc[Object.keys(shc)[1]];
-    var c = shc[Object.keys(shc)[2]];
+// //color mean
+// function colorMean(color1, color2) {
+//     console.log(color1, color2);
+//     rgb = hexToRgb(color1)
+//     shc = hexToRgb(color2)
+//         console.log("rgb", rgb)
+//         console.log("shc", shc)
+//     var r = rgb[Object.keys(rgb)[0]];
+//     var g = rgb[Object.keys(rgb)[1]];
+//     var b = rgb[Object.keys(rgb)[2]];
+//     var s = shc[Object.keys(shc)[0]];
+//     var h = shc[Object.keys(shc)[1]];
+//     var c = shc[Object.keys(shc)[2]];
 
-    t = Math.round((r + s) / 2)
-    i = Math.round((g + h) / 2)
-    d = Math.round((b + c) / 2)
-        console.log("t", t, "i", i, "d", d)
-        console.log("rgbToHex(t, i, d)", rgbToHex(t, i, d))
-    return rgbToHex(t, i, d)
-}
-
-
-//PLAY WITH COLOR
-//http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-
-function hexToRgb(hex) {
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    console.log("hex", hex)
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
-
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-}
+//     t = Math.round((r + s) / 2)
+//     i = Math.round((g + h) / 2)
+//     d = Math.round((b + c) / 2)
+//         console.log("t", t, "i", i, "d", d)
+//         console.log("rgbToHex(t, i, d)", rgbToHex(t, i, d))
+//     return rgbToHex(t, i, d)
+// }
 
 
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
+// //PLAY WITH COLOR
+// //http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
 
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
+// function hexToRgb(hex) {
+//     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+//     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+//     console.log("hex", hex)
+//     hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+//         return r + r + g + g + b + b;
+//     });
+
+//     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+//     return result ? {
+//         r: parseInt(result[1], 16),
+//         g: parseInt(result[2], 16),
+//         b: parseInt(result[3], 16)
+//     } : null;
+// // }
+
+
+// function componentToHex(c) {
+//     var hex = c.toString(16);
+//     return hex.length == 1 ? "0" + hex : hex;
+// }
+
+// function rgbToHex(r, g, b) {
+//     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+// }
 
 
 
-function wait(ms) {
-    var start = new Date().getTime();
-    var end = start;
-    while (end < start + ms) {
-        end = new Date().getTime();
-    }
-}
-//find whats relevant
+// function wait(ms) {
+//     var start = new Date().getTime();
+//     var end = start;
+//     while (end < start + ms) {
+//         end = new Date().getTime();
+//     }
+// }
+// //find whats relevant
 
-//STAT FUNCTIONS
-function standardDeviation(values) {
-    var avg = average(values);
+// //STAT FUNCTIONS
+// function standardDeviation(values) {
+//     var avg = average(values);
 
-    var squareDiffs = values.map(function(value) {
-        var diff = value - avg;
-        var sqrDiff = diff * diff;
-        return sqrDiff;
-    });
+//     var squareDiffs = values.map(function(value) {
+//         var diff = value - avg;
+//         var sqrDiff = diff * diff;
+//         return sqrDiff;
+//     });
 
-    var avgSquareDiff = average(squareDiffs);
+//     var avgSquareDiff = average(squareDiffs);
 
-    var stdDev = Math.sqrt(avgSquareDiff);
-    return stdDev;
-}
+//     var stdDev = Math.sqrt(avgSquareDiff);
+//     return stdDev;
+// }
 
-function average(data) {
-    var sum = data.reduce(function(sum, value) {
-        return sum + value;
-    }, 0);
+// function average(data) {
+//     var sum = data.reduce(function(sum, value) {
+//         return sum + value;
+//     }, 0);
 
-    var avg = sum / data.length;
-    return avg;
-}
+//     var avg = sum / data.length;
+//     return avg;
+// }
