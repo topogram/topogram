8d84a9b5ca6b7f22ceed430df95ce2c62270ca5c
Clément Renaud <clement.renaud@gmail.com>
2016-01-16 13:19:34 +0100

    start basic graph state -- see #15

--- client/network/NetworkGraph.js
+++ client/network/NetworkGraph.js
@@ -507,60 +507,13 @@ NetworkGraph = {
             // self.changeLayout("map");
         });
 
+    },
+
+    // networkState describes the current state of diplay of a specific network
+    graphState: {
+      showNodesLabels : 1,
+      showEdgesLabels : 0,
+      layout : "circle"
     }
 
-
 };
-
-
-// //color mean
-// function colorMean(color1, color2) {
-//     console.log(color1, color2);
-//     rgb = hexToRgb(color1)
-//     shc = hexToRgb(color2)
-//     console.log("rgb", rgb)
-//     console.log("shc", shc)
-//     var r = rgb[Object.keys(rgb)[0]];
-//     var g = rgb[Object.keys(rgb)[1]];
-//     var b = rgb[Object.keys(rgb)[2]];
-//     var s = shc[Object.keys(shc)[0]];
-//     var h = shc[Object.keys(shc)[1]];
-//     var c = shc[Object.keys(shc)[2]];
-
-//     t = Math.round((r + s) / 2)
-//     i = Math.round((g + h) / 2)
-//     d = Math.round((b + c) / 2)
-//     console.log("t", t, "i", i, "d", d)
-//     console.log("rgbToHex(t, i, d)", rgbToHex(t, i, d))
-//     return rgbToHex(t, i, d)
-// }
-
-
-// //Play with color
-// //http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-
-// function hexToRgb(hex) {
-//     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-//     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-//     console.log("hex", hex)
-//     hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-//         return r + r + g + g + b + b;
-//     });
-
-//     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-//     return result ? {
-//         r: parseInt(result[1], 16),
-//         g: parseInt(result[2], 16),
-//         b: parseInt(result[3], 16)
-//     } : null;
-// }
-
-
-// function componentToHex(c) {
-//     var hex = c.toString(16);
-//     return hex.length == 1 ? "0" + hex : hex;
-// }
-
-// function rgbToHex(r, g, b) {
-//     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-// }
