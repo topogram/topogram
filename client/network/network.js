dd7a42868872993068fd1b43e50a3e95ba4cded1
Clément Renaud <clement.renaud@gmail.com>
2016-01-19 17:04:17 +0100

    rewrite network into Meteor template

--- client/network/network.js
+++ client/network/network.js
@@ -1,28 +1,78 @@
-Template.networkTemplate.created = function() {
-    this.network = new ReactiveVar();
-    this.changeLayout = new ReactiveVar();
+Template.network.created = function() {
+  console.log("init network");
 
-    // init on start
-    Session.set( 'currentId', null );
-    Session.set( 'currentType', null );
+  // constants
+  this.colors = d3.scale.category20c();
+  this.editMode = this.data.editMode;
 
+  // fetch data
+  var nodes = Nodes.find().fetch(),
+      edges = Edges.find().fetch();
+
+  // init node/edge selector
+  $('#infoBox').css('visibility', 'hidden'); // hide infobox by default
+  Session.set( 'currentId', null );
+  Session.set( 'currentType', null );
+
+  // node merger
+  Session.set("mergeSource", null)
+  Session.set("mergeTargets", null)
+
+  // init graph state (TODO : should be reactiveDict)
+  this.graphState = {
+    showNodesLabels : 1,
+    showEdgesLabels : 0,
+    layout : "circle",
+    nodeFontSize : 16
+  };
 };
 
-Template.networkTemplate.helpers({
-  hasGeo: function() {
-    return hasGeo()
-  }
+Template.network.destroyed = function() {
+  console.log("destroy network");
+};
+
+
+Template.network.helpers({
+
 });
 
-Template.networkTemplate.rendered = function() {
+Template.network.rendered = function() {
     var self = this;
 
-    // hide infobox by default
-    $('#infoBox').css('visibility', 'hidden');
+    // init graph
+    this.graph = cytoscape({
+        container: document.getElementById("network"),
+        hideLabelsOnViewport: true,
+        ready: function() {
+          // console.log('topogram ready');
+        },
+        // load existing positions
+        layout: {
+            name: 'preset'
+        },
+        // style
+        style: cytoscape.stylesheet()
+            .selector('node')
+              .style({
+                'font-size': this.graphState.fontSize,
+                'text-valign': 'top',
+                'color': 'black',
+                'text-max-width': 100,
+                'text-wrap': 'wrap',
+                'min-zoomed-font-size': 0.4,
+                'background-color' : "#CCC"
+              })
+            // node with degree zero
+            .selector('node[[degree = 0]]')
+              .style({
+                  'background-color': '#555'
+              })
+            .selector('edge')
+              .style({
+                'background-color' : "#000"
+              })
+      });
 
-    // create graph// network.destroy();
-    var topogramId = this.data.topogramId,
-        network = NetworkGraph.initTopogram('network', topogramId, this.data.editMode);
     // fetch and parse data
     var edges = Edges.find().fetch(),
         nodes = Nodes.find().fetch();
@@ -30,23 +80,169 @@ Template.networkTemplate.rendered = function() {
     console.log("edges", edges.length)
 
     // init data
-    if (network) network.initData(nodes, edges);
-    if(!this.data.editMode) network.net.autolock(true);
+    this.graph.elements().remove(); // make sure evything is clean
+    this.graph.add(nodes); // prevent edges to be added before nodes
+    this.graph.add(edges);
+    this.graph.reset(); // render layout
 
+    console.log(this.graph);
 
-    var fieldFunctionMap = {
-        'data': function(elem, data) {
-            elem.data(data);
-        },
-        'position': function(elem, data) {
-            // console.log( elem, data );
-            elem.position(data);
+    // mouse actions
+    this.graph.on('select', 'node', /*_.debounce(*/ function(e) {
+
+        var node = e.cyTarget;
+        console.log(node.data());
+        Session.set('currentType', 'node');
+        Session.set('currentId', node.id());
+
+        // color focus
+        self.graph.nodes().style({
+            'opacity': '.1'
+        });
+        self.graph.edges().style({
+            'opacity': '.1'
+        });
+        node.style({
+            'opacity': '1'
+        });
+        node.neighborhood().style({
+            'opacity': '1'
+        });
+
+        // make only the focus selectable
+        self.graph.nodes().unselectify();
+        self.graph.edges().unselectify(false);
+        node.neighborhood().selectify();
+
+        $('#infoBox').css('visibility', 'visible');
+    });
+
+    this.graph.on('select', 'edge', /*_.debounce(*/ function(e) {
+        var edge = e.cyTarget;
+        Session.set('currentType', 'edge');
+        Session.set('currentId', edge.id());
+        $('#infoBox').css('visibility', 'visible');
+    });
+
+    // drag node
+    this.graph.on('free', 'node', /*_.debounce(*/ function(e) {
+        var node = e.cyTarget;
+
+        // update position
+        Meteor.call('updateNodePosition', node.id(), node.position());
+
+        // Node Merger
+        // if (nodeEditMode == true) {
+        //     // check for node merger
+        //     console.log("check for node merger")
+        //     var bb = node.boundingbox();
+        //
+        //     var targets = Nodes.find({
+        //         "position.x": {
+        //             "$lt": Math.max(bb.x1, bb.x2),
+        //             "$gte": Math.min(bb.x1, bb.x2)
+        //         },
+        //         "position.y": {
+        //             "$lt": Math.max(bb.y1, bb.y2),
+        //             "$gte": Math.min(bb.y1, bb.y2)
+        //         },
+        //         "data.id": {
+        //             "$not": node.id()
+        //         }
+        //     }).fetch();
+        //
+        //     var nodeSource = Nodes.findOne({
+        //         "data.id": node.id()
+        //     });
+        //
+        //     if (targets.length) {
+        //         Session.set("mergeSource", nodeSource)
+        //         Session.set("mergeTargets", targets)
+        //         $('#modal-merge').openModal();
+        //     }
+        // };
+    });
+
+    // interactive edge creation
+    // if(self.editMode)
+    this.graph.edgehandles({
+        complete: function(source, target, addedEntities) {
+            Meteor.call('addEdgeFromIds', self.topogramId, source.data('id'), target.data('id'));
         }
-    };
+    });
+    this.graph.edgehandles("disable"); // disbaled by default
 
-    Template.instance().network.set(network);
+    // qtip
+    this.graph.elements('node:selectable').qtip({
+        content: function() {
+            return this.data().data.type + ' - ' + this.data().data.name;
+        },
+        show: {
+            event: 'mouseover'
+        },
+        hide: {
+            event: 'mouseout'
+        }
+    });
 
+    this.graph.elements('edge:selectable').qtip({
+        content: function() {
+            return this.data().data.type;
+        },
+        show: {
+            event: 'mouseover'
+        },
+        hide: {
+            event: 'mouseout'
+        }
+    });
 
+    // context menu (right click)
+    // if(self.editMode)
+    this.graph.cxtmenu({
+        selector: 'node',
+        commands: [{
+            content: '<span><i class="small material-icons">delete</i></span>',
+            select: function() {
+
+                // remove the node plus all connected edges
+                Meteor.call('deleteNodeAndConnectedEdges', this.id(), this.neighborhood('edge').map(function(d) {
+                    return d.id()
+                }));
+
+                // remove from graph
+                self.graph.remove(this.neighborhood('edge'));
+                self.graph.remove(this);
+            }
+        }, {
+            content: '<span><i class="small material-icons">star_rate</i></span>',
+            select: function() {
+                Meteor.call('starNode', this.id());
+                this.data().starred = (this.data().starred) ? false : true;
+                // console.log("starred", this.data("starred"), this.data("color"));
+                this.style( {
+                  'background-color': function(e){
+                    return e.data("starred") ? "yellow" : e.data("color");
+                  }
+                });
+
+            }
+        }, {
+            content: '<span><i class="small material-icons">lock</i></span>',
+            select: function() {
+                // console.log( this.position() );
+                Meteor.call('lockNode', this.id(), this.position());
+            },
+        }, {
+            content: '<span><i class="small material-icons">comment</i></span>',
+            select: function() {
+                Meteor.call('addComment', this.id());
+            },
+
+        }]
+    });
+
+    // if(self.editMode)
     // watch changes
     /*
     nodes.observeChanges( {
@@ -93,9 +289,3 @@ Template.networkTemplate.rendered = function() {
 Template.networkTemplate.events = {
 
 };
-
-Template.networkTemplate.onDestroyed(function() {
-    // this.network.net.destroy();
-
-    // console.log( 'network destroyed', this.network );
-});
