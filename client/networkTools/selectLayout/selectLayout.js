1eeb53d3ef2df12db1e29058f0cc28517391ad7d
Clément Renaud <clement.renaud@gmail.com>
2016-02-11 14:07:47 +0100

    makeLayout client side pass the right attributes

--- client/networkTools/selectLayout/selectLayout.js
+++ client/networkTools/selectLayout/selectLayout.js
@@ -1,28 +1,3 @@
-Template.selectLayout.events = {
-  // apply layout
-  'click .layout': function(e, template) {
-    var layoutName = $(e.target).data().layoutName;
-    var network = template.view.parentView.parentView._templateInstance.network.get()
-    var layoutConfig = {
-        name: layoutName,
-        stop: function() {  // callback on layoutstop
-            console.log( 'update position' );
-            // var nodesLayout = self.graph.nodes().map(function(node) {
-            //     return {
-            //         id: node.id(),
-            //         position: node.position()
-            //     };
-            // });
-            // Meteor.call('updateNodesPositions', nodesLayout);
-        }
-    };
-
-    var layout = network.makeLayout(layoutConfig);
-    // console.log(network);
-    layout.run();
-  }
-}
-
 Template.selectLayout.helpers({
 
     layouts: function() {
@@ -30,10 +5,19 @@ Template.selectLayout.helpers({
       // add map layout
       if ( hasGeo() ) layouts.push("map");
 
-      layouts.push('random', 'grid', 'circle',
-        // 'arbor', 'cola', 'cose-bilkent', 'dagre',
-        // 'spread',
-        'breadthfirst', 'concentric')
+      layouts.push(
+        'random',
+        'grid',
+        'circle',
+        'springy',
+        // 'arbor',
+        // 'cola',
+        // 'cose-bilkent',
+        //'dagre',
+        'spread',
+        'breadthfirst',
+        'concentric'
+      )
 
       return layouts.map(function(d) {
             return {
@@ -44,3 +28,41 @@ Template.selectLayout.helpers({
     }
 
 });
+
+Template.selectLayout.events = {
+  // apply layout
+  'click .layout': function(e, template) {
+    var layoutName = $(e.target).data().layoutName;
+    var network = template.view.parentView.parentView._templateInstance.network.get()
+
+    console.log(layoutName);
+
+    if(layoutName == "cola" || layoutName == "arbor" || layoutName == "spread" || layoutName == "springy" || layoutName == "cose-bilkent" ) {
+      // var npmLayoutModule = ("cytoscape-"+layoutName).toCamelCase();
+      // var layout = window[npmLayoutModule]; // get lib from global scope
+      // console.log(network, layout, npmLayoutModule);
+      // layout(network) // register layout
+    }
+
+
+    var layoutConfig = {
+        name: layoutName,
+        stop: function() {  // callback on layoutstop
+            console.log( 'update position' );
+            console.log(network.nodes().length);
+            var nodesLayout = network.nodes().map(function(node) {
+                return {
+                    _id : node.data("_id"),
+                    position: node.position()
+                };
+            });
+            console.log(nodesLayout.length, nodesLayout);
+            Meteor.call('updateNodesPositions', nodesLayout); // save nodes position
+        }
+    };
+
+    var layout = network.makeLayout(layoutConfig);
+    // console.log(network);
+    layout.run();
+  }
+}
