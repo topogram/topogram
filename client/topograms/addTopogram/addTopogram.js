a60174af3d4a7e6822608da558c02990278be246
Clément Renaud <clement.renaud@gmail.com>
2016-02-16 12:04:25 +0100

    better UI for topogram creation

--- client/topograms/addTopogram/addTopogram.js
+++ client/topograms/addTopogram/addTopogram.js
@@ -1,9 +1,13 @@
 Template.addTopogram.events( {
     'submit form': function( event ) {
         event.preventDefault();
-        var topogramName = $( '[name=topogramName]' ).val();
+        var topogramName = $( 'input[name="topogramName"]' ).val();
+        console.log(topogramName);
         if ( topogramName != '' ) {
-            Meteor.call( 'createTopogram', Meteor.userId(), topogramName );
+            Meteor.call( 'createTopogram', Meteor.userId(), topogramName, function(err, topogram){
+              Router.go( '/topograms/' + topogram + '/import' );
+            });
+
             $( '[name=topogramName]' ).val( '' );
         } else {
             FlashMessages.sendError( 'TopogramName should not be empty' );
