4edd72873845c435f1c4fe5db6f7eccafd45a9f2
Clément Renaud <clement.renaud@gmail.com>
2016-02-16 17:50:29 +0100

    truncate string function

--- client/helpers.js
+++ client/helpers.js
@@ -6,3 +6,10 @@ Template.registerHelper( 'objectToPairs', function( object ) {
         };
     } );
 } );
+
+// truncate String to make it shorter
+String.prototype.trunc = function(m) {
+  return (this.length > m)
+    ? jQuery.trim(this).substring(0, m).split(" ").slice(0, -1).join(" ") + "..."
+    : this;
+};
