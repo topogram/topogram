a9f4d9e21f97b724b1e84bd4ab870042e4e4ea54
Clément Renaud <clement.renaud@gmail.com>
2016-05-14 10:21:53 +0200

    add relationships  pie chat

--- client/boxes/infobox/nodeInfo/nodeInfo.js
+++ client/boxes/infobox/nodeInfo/nodeInfo.js
@@ -10,5 +10,27 @@ Template.nodeInfo.helpers({
       var neighborhood = network.nodes().filter("[id='"+node.id+"']").neighborhood();
       return neighborhood.nodes().map(function(d){ return d.data()});
     }
+  },
+  neighborhoodTypeCount: function() {
+
+    var node = Template.instance().data.node;
+    var network = Template.instance().data.network
+
+    if(node) {
+      var neighborhood = network.nodes().filter("[id='"+node.id+"']").neighborhood();
+
+      var count = neighborhood.nodes()
+        .map(function(d){ return d.data("group")})
+        .reduce(function(map, type){
+          map[type] = (map[type]||0)+1;
+          return map;
+        }, Object.create(null));
+
+      return Object.keys(count).map(function(key){
+        return { "group" : key, "count": count[key], "color": colors(key) }
+      })
+    }
+
   }
+
 })
