cf0e7e4b3592687a045bec2c837fac0fccaecba7
Clément Renaud <clement.renaud@gmail.com>
2016-06-02 09:11:18 +0200

    cleaner filterbox

--- client/boxes/filterbox/filterbox.js
+++ client/boxes/filterbox/filterbox.js
@@ -1,8 +1,12 @@
-Template.filters.events({
+Template.filterbox.events({
   'click .clear-filters' : function(e, template) {
     console.log(template);
     var net = template.view.parentView.parentView._templateInstance.network.get();
     console.log(net);
     net.resetFilters();
+  },
+  "click .close-filters" : function(e) {
+    console.log("close");
+    $("#filterbox").hide();
   }
 })
