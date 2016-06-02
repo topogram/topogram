207512b7e20dcb7022e67105175ee17b5ce4fd91
Clément Renaud <clement.renaud@gmail.com>
2016-06-02 09:46:19 +0200

    remove side menu and simplify nav

--- client/partials/header.js
+++ client/partials/header.js
@@ -1,14 +1,25 @@
-Template.menu.helpers({
-  topograms : function() {
-    return Topograms.find().fetch()
+Template.header.helpers({
+  isLoggedIn : function() {
+    console.log(Meteor.userId());
+    console.log("ha");
+    return Meteor.userId() ? true : false;
   }
 })
 
+Template.header.events = {
+  "click #logout": function() {
+      Meteor.logout(function(err) {
+        // callback
+        console.log("logged out");
+    });
+  }
+}
+
 Template.header.rendered = function() {
   // sidebar
-  $('#menu-trigger').sideNav({
-    closeOnClick: true,
-    menuWidth: 300, // Default is 240
-    edge: 'left' // Choose the horizontal origin
-  });
+  // $('#menu-trigger').sideNav({
+  //   closeOnClick: true,
+  //   menuWidth: 300, // Default is 240
+  //   edge: 'left' // Choose the horizontal origin
+  // });
 }
