abbc052c3e553c7b328cad0d69337405f4f42e95
Clément Renaud <clement.renaud@gmail.com>
2016-06-07 19:07:55 +0200

    remove unused demo tests

--- tests/jasmine/client/unit/sample/src/Song.js
+++
@@ -1,8 +0,0 @@
-/* globals Song: true */
-
-Song = function Song() {};
-
-Song.prototype.persistFavoriteStatus = function() {
-  // something complicated
-  throw new Error('not yet implemented');
-};
