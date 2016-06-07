4484ea6228a68b418053c3f4f62f75842125c7a5
Clément Renaud <clement.renaud@gmail.com>
2016-06-07 17:17:40 +0200

    init REST API with restivus - adress #73

---
+++ server/api.js
@@ -0,0 +1,44 @@
+// Global API configuration
+ var Api = new Restivus({
+  apiPath: "api",
+  useDefaultAuth: true,
+  prettyJson: true,
+  onLoggedIn: function () {
+    console.log(this.user.username + ' (' + this.userId + ') logged in');
+  },
+  onLoggedOut: function () {
+    console.log(this.user.username + ' (' + this.userId + ') logged out');
+  }
+ });
+
+ // Generates: GET, POST on /api/items and GET, PUT, DELETE on
+ // /api/items/:id for the Items collection
+ Api.addCollection(Topograms, {
+   routeOptions: {
+     authRequired: true
+   },
+   endpoints: {
+     getAll: {
+       action: function () {
+         return Topograms.find({ "owner": this.userId}).fetch();
+       }
+     }
+  }
+});
+
+ // Generates: POST on /api/users and GET, DELETE /api/users/:id for
+ // Meteor.users collection
+ Api.addCollection(Meteor.users, {
+   excludedEndpoints: ['getAll', 'put'],
+   routeOptions: {
+     authRequired: true
+   },
+   endpoints: {
+     post: {
+       authRequired: false
+     },
+     delete: {
+       roleRequired: 'admin'
+     }
+   }
+ });
