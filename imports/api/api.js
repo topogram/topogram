daa98c688d3cc8e1957fe6134e1ea9b4e795de7b
Clément Renaud <clement.renaud@gmail.com>
2016-06-23 18:12:26 +0200

    ignore 'meteor/XXX' import errors in eslint

--- imports/api/api.js
+++ imports/api/api.js
@@ -1,15 +1,21 @@
+import { Meteor } from 'meteor/meteor'
+import { Restivus } from 'meteor/nimble:restivus'
+import { Topograms } from './collections.js'
+import { logger } from '../logger.js'
+
+
 // Global API configuration
  var Api = new Restivus({
   apiPath: "api",
   useDefaultAuth: true,
   prettyJson: true,
   onLoggedIn: function () {
-    console.log(this.user.username + ' (' + this.userId + ') logged in') 
+    logger.log(this.user.username + ' (' + this.userId + ') logged in')
   },
   onLoggedOut: function () {
-    console.log(this.user.username + ' (' + this.userId + ') logged out') 
+    logger.log(this.user.username + ' (' + this.userId + ') logged out')
   }
- }) 
+ })
 
  // Generates: GET, POST on /api/items and GET, PUT, DELETE on
  // /api/items/:id for the Items collection
@@ -20,11 +26,11 @@
    endpoints: {
      getAll: {
        action: function () {
-         return Topograms.find({ "owner": this.userId}).fetch() 
+         return Topograms.find({ "owner": this.userId}).fetch()
        }
      }
   }
-}) 
+})
 
  // Generates: POST on /api/users and GET, DELETE /api/users/:id for
  // Meteor.users collection
@@ -41,4 +47,4 @@
        roleRequired: 'admin'
      }
    }
- }) 
+ })
