bd277c0f1bbb289228743362481e960f5b88b7c7
Clément Renaud <clement.renaud@gmail.com>
2016-06-23 17:46:25 +0200

    lint fix for nodes/edges options

--- imports/ui/components/networkTools/edgesOptions/edgesOptions.js
+++ imports/ui/components/networkTools/edgesOptions/edgesOptions.js
@@ -1,3 +1,6 @@
+import './edgesOptions.html'
+import { Template } from 'meteor/templating'
+
 Template.edgesOptions.helpers({
   edgeWeightMethods: function() {
       return ["simple", "weight"]
@@ -7,22 +10,22 @@ Template.edgesOptions.helpers({
   },
   edgeHasWeight : function() {
     if (!edge) return false
-    else var edge = Edges.findOne() 
-    return (edge.data.width || edge.data.weight ) ? true: false 
+    else var edge = Edges.findOne()
+    return (edge.data.width || edge.data.weight ) ? true: false
   },
   edgeHasProperties : function() {
     if (!edge) return false
-    else var edge = Edges.findOne() 
-    return (edge.data.width || edge.data.weight || edge.data.type || edge.data.color ) ? true: false 
+    else var edge = Edges.findOne()
+    return (edge.data.width || edge.data.weight || edge.data.type || edge.data.color ) ? true: false
   }
 
 })
 
 Template.edgesOptions.events = {
   'change #edgeWidthMethod': function(e, template) {
-      var net = template.view.parentView.parentView._templateInstance.network.get() 
-      var val = $(e.currentTarget).find('option:selected').val() 
-      var self = this 
+      var net = template.view.parentView.parentView._templateInstance.network.get()
+      var val = $(e.currentTarget).find('option:selected').val()
+      var self = this
 
       if (val == 'simple') {
           net.edges().style({ 'width': 5 })
@@ -35,11 +38,11 @@ Template.edgesOptions.events = {
       }
   },
   'change #edgeEndMethod': function(e, template) {
-      var net = template.view.parentView.parentView._templateInstance.network.get() 
-      var val = $(e.currentTarget).find('option:selected').val() 
+      var net = template.view.parentView.parentView._templateInstance.network.get()
+      var val = $(e.currentTarget).find('option:selected').val()
       var nodes = Nodes.find().fetch(),
-          edges = Edges.find().fetch() 
-      var self = this 
+          edges = Edges.find().fetch()
+      var self = this
 
       if (val == 'simple') {
           net.edges().forEach(function(ele) {
@@ -65,11 +68,11 @@ Template.edgesOptions.events = {
       }
   },
   'change #edgesColor': function(e, template) {
-    var net = template.view.parentView.parentView._templateInstance.network.get() 
-    console.log($(e.currentTarget).find('option:selected')) 
-    var edgeColorMode = $(e.currentTarget).find('option:selected').val() 
-    console.log(edgeColorMode) 
-    var self = this 
+    var net = template.view.parentView.parentView._templateInstance.network.get()
+    console.log($(e.currentTarget).find('option:selected'))
+    var edgeColorMode = $(e.currentTarget).find('option:selected').val()
+    console.log(edgeColorMode)
+    var self = this
     if (val == 'file') {
         // console.log("I'm in edgefile")
         net.edges().forEach(function(ele) {
@@ -95,13 +98,13 @@ Template.edgesOptions.events = {
        console.log("I'm in!3")
         net.edges().forEach(function(ele) {
             if (ele.data().group == 0) {
-                FlashMessages.sendError("no data available, creating") 
+                FlashMessages.sendError("no data available, creating")
                 //WE SET THIS BECAUSE WE WANT TO BE ABLE TO DISTINGUISH BETWEEN SELF AND ADDED COLORS, SO WE DON4T RETUR ele.data.color
                 ele.data("group", colorsEdge(ele.data().id))
             }
 
             if (ele.data().color == 0) {
-                FlashMessages.sendError("no color available, creating") 
+                FlashMessages.sendError("no color available, creating")
                 //WE SET THIS BECAUSE WE WANT TO BE ABLE TO DISTINGUISH BETWEEN SELF AND ADDED COLORS, SO WE DON4T RETUR ele.data.color
                 ele.data("color", colorsEdge(ele.data().group))
             }
@@ -111,55 +114,55 @@ Template.edgesOptions.events = {
         })
     } else if (val == 'nodesMeanGroup') {
         net.edges().forEach(function(ele) {
-            var sourceNode = "" 
-            var targetNode = "" 
-            srcFound = false 
-            tarFound = false 
-            for (var i = 0, l = nodes.length  i < l  i++) {
-                if (!srcFound && nodes[i].data.id == ele.data().source) {
-                    sourceNode = nodes[i] 
-                    srcFound = true 
-                } else if (!tarFound && nodes[i].data.id == ele.data().target) {
-                    targetNode = nodes[i] 
-                    tarFound = true 
+            var sourceNode = ""
+            var targetNode = ""
+            srcFound = false
+            tarFound = false
+            for (var node of nodes) {
+                if (!srcFound && node.data.id == ele.data().source) {
+                    sourceNode = node
+                    srcFound = true
+                } else if (!tarFound && node.data.id == ele.data().target) {
+                    targetNode = node
+                    tarFound = true
                 } else if (srcFound && tarFound) break  //stop for-loop since we found our nodes
             }
-            console.log("sourceNode", sourceNode.data.id) 
-            console.log("targetNode", targetNode.data.id) 
-            console.log("sourceNode color", sourceNode.data.color) 
-            console.log("sourceNode group", sourceNode.data.group) 
-            console.log("targetNode color", targetNode.data.color) 
-            console.log("targetNode group", targetNode.data.color) 
-            var color = colorMean(colorsNode(sourceNode.data.group), colorsNode(targetNode.data.group)) 
-            console.log("color",color) 
+            console.log("sourceNode", sourceNode.data.id)
+            console.log("targetNode", targetNode.data.id)
+            console.log("sourceNode color", sourceNode.data.color)
+            console.log("sourceNode group", sourceNode.data.group)
+            console.log("targetNode color", targetNode.data.color)
+            console.log("targetNode group", targetNode.data.color)
+            var color = colorMean(colorsNode(sourceNode.data.group), colorsNode(targetNode.data.group))
+            console.log("color",color)
             ele.style({
                 'line-color': ele.data('starred') ? 'yellow' : color,
                 'line-style': "solid"
             })
         })
     } else if (val == 'nodesMeanColor') {
-        net.edges().forEach(function(ele) {
-            var sourceNode = "" 
-            var targetNode = "" 
-            srcFound = false 
-            tarFound = false 
-            for (var i = 0, l = nodes.length  i < l  i++) {
-                if (!srcFound && nodes[i].data.id == ele.data().source) {
-                    sourceNode = nodes[i] 
-                    srcFound = true 
-                } else if (!tarFound && nodes[i].data.id == ele.data().target) {
-                    targetNode = nodes[i] 
-                    tarFound = true 
+        net.edges().forEach( function(ele)  {
+            var sourceNode = ""
+            var targetNode = ""
+            srcFound = false
+            tarFound = false
+            for (var node in nodes) {
+                if (!srcFound && node.data.id == ele.data().source) {
+                    sourceNode = node
+                    srcFound = true
+                } else if (!tarFound && node.data.id == ele.data().target) {
+                    targetNode = node
+                    tarFound = true
                 } else if (srcFound && tarFound) break  //stop for-loop since we found our nodes
             }
-            console.log("sourceNode", sourceNode.data.id) 
-            console.log("targetNode", targetNode.data.id) 
-            console.log("sourceNode color", sourceNode.data.color) 
-            console.log("sourceNode group", sourceNode.data.group) 
-            console.log("targetNode color", targetNode.data.color) 
-            console.log("targetNode group", targetNode.data.color) 
-            var color = colorMean(sourceNode.data.color, targetNode.data.color) 
-            console.log("color",color) 
+            console.log("sourceNode", sourceNode.data.id)
+            console.log("targetNode", targetNode.data.id)
+            console.log("sourceNode color", sourceNode.data.color)
+            console.log("sourceNode group", sourceNode.data.group)
+            console.log("targetNode color", targetNode.data.color)
+            console.log("targetNode group", targetNode.data.color)
+            var color = colorMean(sourceNode.data.color, targetNode.data.color)
+            console.log("color",color)
             ele.style({
                 'line-color': ele.data('starred') ? 'yellow' : color,
                 'line-style': "solid"
@@ -169,20 +172,20 @@ Template.edgesOptions.events = {
         net.edges().forEach(function(ele) {
             var sourceNode, targetNode,
                 srcFound = false,
-                tarFound = false 
-            for (var i = 0, l = nodes.length  i < l  i++) {
-                if (!srcFound && nodes[i].data.id == ele.data().source) {
-                    sourceNode = nodes[i] 
-                    srcFound = true 
-                } else if (!tarFound && nodes[i].data.id == ele.data().target) {
-                    targetNode = nodes[i] 
-                    tarFound = true 
+                tarFound = false
+            for (var node in nodes) {
+                if (!srcFound && node.data.id == ele.data().source) {
+                    sourceNode = node
+                    srcFound = true
+                } else if (!tarFound && node.data.id == ele.data().target) {
+                    targetNode = node
+                    tarFound = true
                 } else if (srcFound && tarFound) break  //stop for-loop since we found our nodes
             }
-            // console.log("sourceNode", sourceNode) 
-            // console.log("targetNode", targetNode) 
-            // console.log("sourceNode color", sourceNode.data.color) 
-            // console.log("targetNode color", targetNode.data.color) 
+            // console.log("sourceNode", sourceNode)
+            // console.log("targetNode", targetNode)
+            // console.log("sourceNode color", sourceNode.data.color)
+            // console.log("targetNode color", targetNode.data.color)
 
             if (sourceNode.data.group != targetNode.data.group) {
                 ele.style({
@@ -207,10 +210,10 @@ Template.edgesOptions.events = {
         net.edges().forEach(function(ele) {
             //FIXME:
             ele.data().count = ele.data().width
-            var width = ele.data().count 
-            var val = parseInt(countEdgeViewParam1.value) 
-            var val2 = parseInt(countEdgeViewParam2.value) 
-            var val3 = parseInt(countEdgeViewParam3.value) 
+            var width = ele.data().count
+            var val = parseInt(countEdgeViewParam1.value)
+            var val2 = parseInt(countEdgeViewParam2.value)
+            var val3 = parseInt(countEdgeViewParam3.value)
             //TODO: D3 SCALE
             // console.log("widthedge", width)
             if (width <= val) {
@@ -240,27 +243,27 @@ Template.edgesOptions.events = {
         net.edges().forEach(function(ele) {
             //FIXME:
             ele.data().count = ele.data().width
-            var width = ele.data().count 
-            var val = parseInt(countEdgeViewParam1.value) 
-            var val2 = parseInt(countEdgeViewParam2.value) 
-            var val3 = parseInt(countEdgeViewParam3.value) 
-            var nodesMeanDeg = 0 
-            var nodesMeanDegtmp = 0 
+            var width = ele.data().count
+            var val = parseInt(countEdgeViewParam1.value)
+            var val2 = parseInt(countEdgeViewParam2.value)
+            var val3 = parseInt(countEdgeViewParam3.value)
+            var nodesMeanDeg = 0
+            var nodesMeanDegtmp = 0
             var sourceNode, targetNode,
                 srcFound = false,
-                tarFound = false 
+                tarFound = false
 
-            for (var i = 0, l = nodes.length  i < l  i++) {
-                if (!srcFound && nodes[i].data.id == ele.data().source) {
-                    sourceNode = nodes[i] 
-                    srcFound = true 
-                } else if (!tarFound && nodes[i].data.id == ele.data().target) {
-                    targetNode = nodes[i] 
-                    tarFound = true 
+            for (var node in nodes) {
+                if (!srcFound && node.data.id == ele.data().source) {
+                    sourceNode = node
+                    srcFound = true
+                } else if (!tarFound && node.data.id == ele.data().target) {
+                    targetNode = node
+                    tarFound = true
                 } else if (srcFound && tarFound) break  //stop for-loop since we found our nodes
             }
-            var sourceDeg = sourceNode.degree() 
-            var targetDeg = targetNode.degree() 
+            var sourceDeg = sourceNode.degree()
+            var targetDeg = targetNode.degree()
 
             //TODO: D3 SCALE
             // console.log("widthedge", width)
@@ -282,32 +285,32 @@ Template.edgesOptions.events = {
         })
     } else if (val == 'sigma') {
         var data1 = []
-        console.log("data1", data1) 
+        console.log("data1", data1)
 
         net.edges().forEach(function(ele) {
             //FIXME
             ele.data().count = ele.data().width
-            data1.push(parseFloat(ele.data().count)) 
-            console.log("ele.data().width, ele.data().count", ele.data().width, ele.data().count) 
+            data1.push(parseFloat(ele.data().count))
+            console.log("ele.data().width, ele.data().count", ele.data().width, ele.data().count)
         })
 
-        console.log("I'm in sigma") 
-        console.log("data1", data1) 
-        var average1 = average(data1) 
-        var standardDeviation1 = standardDeviation(data1) 
-        console.log("average1", average1) 
-        console.log("standardDeviation1", standardDeviation1) 
-        var val = parseInt(countEdgeViewParam1.value) 
+        console.log("I'm in sigma")
+        console.log("data1", data1)
+        var average1 = average(data1)
+        var standardDeviation1 = standardDeviation(data1)
+        console.log("average1", average1)
+        console.log("standardDeviation1", standardDeviation1)
+        var val = parseInt(countEdgeViewParam1.value)
         // NOT SURE IT IS NEEDED
         if (val > 6) {
-            val = 1 
+            val = 1
             console.log("sigma value set too high, so taking 1 instead")
         }
-        console.log("val", val) 
+        console.log("val", val)
         net.edges().forEach(function(ele) {
             //FIXME:
             ele.data().count = ele.data().width
-            var width = ele.data().count 
+            var width = ele.data().count
             //TODO: D3 SCALE
 
             if (width <= (average1 - ((val + 4) * standardDeviation1)) || width >= (average1 + ((val + 4) * standardDeviation1))) {
@@ -330,9 +333,9 @@ Template.edgesOptions.events = {
                 zIndex = 4
             } else {
                 console.log("FOR UNCLASSIFIEDS")
-                console.log(width) 
-                console.log("average1", average1) 
-                console.log("standardDeviation1", standardDeviation1) 
+                console.log(width)
+                console.log("average1", average1)
+                console.log("standardDeviation1", standardDeviation1)
                 color = '#000000'
             }
             ele.style({
--- imports/ui/components/networkTools/nodesOptions/nodesOptions.js
+++ imports/ui/components/networkTools/nodesOptions/nodesOptions.js
@@ -1,27 +1,30 @@
+import './nodesOptions.html'
+import { Template } from 'meteor/templating'
+
 Template.nodesOptions.helpers({
   nodeColorMethod: function() {
       return ["fix", "setInFile", "group", "alphabet", "count", "compNodEdg", "sigma", "sigmaDegree"]
   },
 
   nodeSizeMethods: function() {
-      var node = Nodes.findOne() 
-      var nodeSizeMethods = ["fixed", "degree"] 
+      var node = Nodes.findOne()
+      var nodeSizeMethods = ["fixed", "degree"]
       if (!node) return false
-      else if (node.data.count) nodeSizeMethods.push("width") 
+      else if (node.data.count) nodeSizeMethods.push("width")
       return nodeSizeMethods
   },
 
-}) 
+})
 
 Template.nodesOptions.events = {
 
   'change #nodeSize': function(e, template) {
-      var net = template.view.parentView.parentView._templateInstance.network.get() 
-      var val = $(e.currentTarget).find('option:selected').val() 
-      var self = this 
+      var net = template.view.parentView.parentView._templateInstance.network.get()
+      var val = $(e.currentTarget).find('option:selected').val()
+      var self = this
 
-      var degrees = net.nodes().nodes().map(function(e){ return e.degree() }) 
-      var radiusScale = d3.scale.linear().domain([d3.min(degrees),d3.max(degrees)]).range([ 5, 40 ]) 
+      var degrees = net.nodes().nodes().map(function(e){ return e.degree() })
+      var radiusScale = d3.scale.linear().domain([d3.min(degrees),d3.max(degrees)]).range([ 5, 40 ])
 
       if (val == 'degree')
           net.nodes().style({
@@ -42,11 +45,11 @@ Template.nodesOptions.events = {
           })
   },
   'change #nodeColorMethod': function(e, template) {
-      var net = template.view.parentView.parentView._templateInstance.network.get() 
-      var val = $(e.currentTarget).find('option:selected').val() 
+      var net = template.view.parentView.parentView._templateInstance.network.get()
+      var val = $(e.currentTarget).find('option:selected').val()
       var nodes = Nodes.find().fetch(),
-          edges = Edges.find().fetch() 
-      var self = this 
+          edges = Edges.find().fetch()
+      var self = this
 
       if (val == 'setInFile') {
           // console.log("I'm in nodefile")
@@ -61,59 +64,34 @@ Template.nodesOptions.events = {
                   })
               }
           })
-      } else if (val == 'alphabet') {
-          // console.log("I'm in! nodealpha")
-          warning = []
-          net.nodes().forEach(function(ele) {
-                  // console.log("ele.data().name", ele.data().name)
-                  if (ele.data().name == "" || 0) {
-                      warning.push(ele.data().id) 
-                      var eleColor = colorsNode(ele.data().id.slice(0, 1))
-                  } else {
-                      var eleColor = colorsNode(ele.data().name.slice(0, 1)) 
-                  }
-                  ele.style({
-                      'background-color': ele.data('starred') ? 'yellow' : eleColor
-                  })
-              })
-              // console.log("warning.length", warning.length)
-
-          if (warning.length > 0) {
-              text = "elements with ids "
-              for (index = 0  index < warning.length  index++) {
-                  text += warning[index] + " " 
-              }
-              text += "used id instead of name field"
-              FlashMessages.sendError(text)
-          }
       } else if (val == 'group') {
           net.nodes().forEach(function(ele) {
               if (ele.data().group == 0 || "") {
-                  FlashMessages.sendError("no data available, creating") 
+                  FlashMessages.sendError("no data available, creating")
                   //WE SET THIS BECAUSE WE WANT TO BE ABLE TO DISTINGUISH BETWEEN SELF AND ADDED COLORS, SO WE DON4T RETUR ele.data.color
-                  var tmpcol = colorsNode(ele.data().id) 
+                  var tmpcol = colorsNode(ele.data().id)
                   // console.log(tmpcol)
                   ele.data({
                       'group': tmpcol
-                  }) 
-                  // console.log("ele.data().group", ele.data().group) 
+                  })
+                  // console.log("ele.data().group", ele.data().group)
               }
 
               if (ele.data().color == 0 || "") {
-                  //FlashMessages.sendError("no color available, creating") 
+                  //FlashMessages.sendError("no color available, creating")
                   //WE SET THIS BECAUSE WE WANT TO BE ABLE TO DISTINGUISH BETWEEN SELF AND ADDED COLORS, SO WE DON4T RETUR ele.data.color
                   var tmpcol = colorsNode(ele.data().group)
                       // console.log(tmpcol)
                   ele.data({
                       'color': tmpcol
-                  }) 
-                  // console.log("ele.data().color", ele.data().color) 
+                  })
+                  // console.log("ele.data().color", ele.data().color)
                   if (ele.data().id == 'BEAL') {
                       // console.log('BEAL', ele.data().color)
                   }
               }
               // console.log(ele.data().color)
-              var eleColor = (ele.data().group == 0) ? "#CCCCCC" : colorsNode(ele.data().group) 
+              var eleColor = (ele.data().group == 0) ? "#CCCCCC" : colorsNode(ele.data().group)
               // console.log(eleColor)
               ele.style({
                   'background-color': ele.data('starred') ? 'yellow' : eleColor
@@ -121,32 +99,32 @@ Template.nodesOptions.events = {
           })
       } else if (val == 'sigma') {
           var data2 = []
-          console.log("data2", data2) 
+          console.log("data2", data2)
 
           net.nodes().forEach(function(ele) {
               //FIXME
               ele.data().count = ele.data().width
-              data2.push(parseFloat(ele.data().count)) 
-              console.log("ele.data().width, ele.data().count", ele.data().width, ele.data().count) 
+              data2.push(parseFloat(ele.data().count))
+              console.log("ele.data().width, ele.data().count", ele.data().width, ele.data().count)
           })
 
-          console.log("I'm in sigma nodeS") 
-          console.log("data2", data2) 
-          var average2 = average(data2) 
-          var standardDeviation2 = standardDeviation(data2) 
-          console.log("average2", average2) 
-          console.log("standardDeviation2", standardDeviation2) 
-          var val = parseInt(countEdgeViewParam1.value) 
+          console.log("I'm in sigma nodeS")
+          console.log("data2", data2)
+          var average2 = average(data2)
+          var standardDeviation2 = standardDeviation(data2)
+          console.log("average2", average2)
+          console.log("standardDeviation2", standardDeviation2)
+          var val = parseInt(countEdgeViewParam1.value)
           // NOT SURE IT IS NEEDED
           if (val > 6) {
-              val = 1 
+              val = 1
               console.log("sigma value set too high, so taking 1 instead")
           }
-          console.log("val", val) 
+          console.log("val", val)
           net.nodes().forEach(function(ele) {
               //FIXME:
               ele.data().count = ele.data().width
-              var width = ele.data().count 
+              var width = ele.data().count
               //TODO: D3 SCALE
 
               if (width <= (average2 - ((val + 4) * standardDeviation2)) || width >= (average2 + ((val + 4) * standardDeviation2))) {
@@ -169,9 +147,9 @@ Template.nodesOptions.events = {
                   zIndex = 4
               } else {
                   console.log("FOR UNCLASSIFIEDS")
-                  console.log(width) 
-                  console.log("average2", average2) 
-                  console.log("standardDeviation2", standardDeviation2) 
+                  console.log(width)
+                  console.log("average2", average2)
+                  console.log("standardDeviation2", standardDeviation2)
                   color = '#000000'
               }
               ele.style({
@@ -181,32 +159,32 @@ Template.nodesOptions.events = {
           })
       } else if (val == 'sigmaDegree') {
           var data2 = []
-          console.log("data2", data2) 
+          console.log("data2", data2)
 
           net.nodes().forEach(function(ele) {
               //FIXME
               //ele.data().count = ele.data().width
-              data2.push(parseFloat(ele.degree())) 
-              console.log("ele.degree()", ele.degree()) 
+              data2.push(parseFloat(ele.degree()))
+              console.log("ele.degree()", ele.degree())
           })
 
-          console.log("I'm in sigma Degree Nodes") 
-          console.log("data2", data2) 
-          var average2 = average(data2) 
-          var standardDeviation2 = standardDeviation(data2) 
-          console.log("average2", average2) 
-          console.log("standardDeviation2", standardDeviation2) 
-          var val = parseInt(countEdgeViewParam1.value) 
+          console.log("I'm in sigma Degree Nodes")
+          console.log("data2", data2)
+          var average2 = average(data2)
+          var standardDeviation2 = standardDeviation(data2)
+          console.log("average2", average2)
+          console.log("standardDeviation2", standardDeviation2)
+          var val = parseInt(countEdgeViewParam1.value)
           // TOBEFIXED:need another use of ranges
           //if (val > 6 ) {
-          val = 1 
+          val = 1
           console.log("sigma value set to fix, so taking 1 instead, need rewrite")
               //}
-          console.log("val", val) 
+          console.log("val", val)
           net.nodes().forEach(function(ele) {
 
               //ele.data().count = ele.data().width
-              var width = ele.degree() 
+              var width = ele.degree()
               //TODO: D3 SCALE
 
               if (width <= (average2 - ((val + 4) * standardDeviation2)) || width >= (average2 + ((val + 4) * standardDeviation2))) {
@@ -229,9 +207,9 @@ Template.nodesOptions.events = {
                   zIndex = 4
               } else {
                   console.log("FOR UNCLASSIFIEDS")
-                  console.log(width) 
-                  console.log("average2", average2) 
-                  console.log("standardDeviation2", standardDeviation2) 
+                  console.log(width)
+                  console.log("average2", average2)
+                  console.log("standardDeviation2", standardDeviation2)
                   color = '#000000'
               }
               ele.style({
