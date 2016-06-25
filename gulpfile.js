849dc0a408e9bddbd14a7254e9b018b7557aa44e
Clément Renaud <clement.renaud@gmail.com>
2016-06-25 14:33:52 +0200

    use phantomjs only in travis

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('test-meteor',
   shell.task(['meteor test --once --driver-package=dispatch:mocha-phantomjs'], { verbose : true})
 )
 
-gulp.task('test-components', function() {
+gulp.task('test', function() {
   gulp.src( './tests/*.js', { read: false } )
     .pipe( mocha( {
       reporter:'nyan',
@@ -28,10 +28,6 @@ gulp.task('test-components', function() {
               js: babel
           }
     } ) )
-})
-
-gulp.task( 'test', [], function(next) {
-  runSequence('test-components', 'test-meteor')
 })
 
 /*
