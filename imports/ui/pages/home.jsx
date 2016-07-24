f5633baf8ff6d33b7cf782cacb148b03ded36f57
Clément Renaud <clement.renaud@gmail.com>
2016-07-24 15:48:47 +0200

    improve home

--- imports/ui/pages/home.jsx
+++ imports/ui/pages/home.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { Topograms } from '../../api/collections.js'
 
-import TopogramAddForm from './topograms/topograms-add.jsx'
-import TopogramPublicList from './home/TopogramPublicList.jsx'
-import HomeHeader from './home/HomeHeader.jsx'
+import TopogramAddForm from '../react/topograms/TopogramAddForm.jsx'
+import TopogramPublicList from '../react/topograms/TopogramPublicList.jsx'
+import HomeHeader from '../react/home/HomeHeader.jsx'
 
 // define and export our Welcome component
 export const Welcome = ({name}) => (
     <div>
       <HomeHeader />
       <TopogramAddForm />
-      <h5 className='gray-text'>Browse existing topograms</h5> 
+      <h5 className='grey-text text-lighten-2 center'>Browse existing topograms</h5>
       <TopogramPublicList />
     </div>
 )
