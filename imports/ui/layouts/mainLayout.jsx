19401773c75a594906838eb01b7a542604a3498e
Clément Renaud <clement.renaud@gmail.com>
2016-07-26 15:11:53 +0200

    custom color theme for material-ui -- feels better

--- imports/ui/layouts/mainLayout.jsx
+++ imports/ui/layouts/mainLayout.jsx
@@ -1,11 +1,26 @@
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import React from 'react'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { green500, indigo700, redA200 } from 'material-ui/styles/colors'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
 import '../css/topogram.scss'
 
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+// injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color : green500,
+    primary2Color : indigo700,
+    accent1Color : redA200,
+    pickerHeaderColor : green500,
+  },
+})
+
 const mainLayout = ({ content }) =>
   (
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
       <div className="main-view container">
         {content}
       </div>
