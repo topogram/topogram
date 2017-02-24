dc2b8d9974d2a35dcd8f88dd3bb78632bccd30bc
Clément Renaud <clement.renaud@gmail.com>
2017-02-24 18:49:15 +0100

    fix typo publications

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -17,9 +17,9 @@ Meteor.publish( 'comments', function (topogramId) {
 
 Meteor.publish( 'edges', function ( topogramId ) {
   return Edges.find({ topogramId })
-})
+});
 
 
 Meteor.publish( 'nodes', function ( topogramId ) {
-  returns Nodes.find({ topogramId })
-})
+  return Nodes.find({ topogramId })
+});
