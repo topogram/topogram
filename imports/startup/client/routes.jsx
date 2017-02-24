4dfc88aa2b23d6156562033dd5e904921f45babf
Clément Renaud <clement.renaud@gmail.com>
2017-02-24 18:15:45 +0100

    clean routes

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -6,6 +6,8 @@ import store from '/imports/client/store';
 
 import MainLayout from '/imports/ui/layouts/MainLayout';
 import { HomeContainer } from '/imports/ui/containers/HomeContainer.jsx'
+import { TopogramContainer } from '/imports/ui/containers/TopogramContainer.jsx'
+import Page404 from '/imports/ui/pages/Page404.jsx'
 
 
 export const renderRoutes = () => (
@@ -13,6 +15,8 @@ export const renderRoutes = () => (
     <Router history={browserHistory}>
       <Route path="/" component={MainLayout}>
         <IndexRoute component={HomeContainer}/>
+        <Route path="topograms/:topogramsId" component={TopogramContainer}/>
+        <Route path="*" component={Page404}/>
       </Route>
     </Router>
   </Provider>
