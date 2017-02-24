93890d8c57b7eac8a22ed88364e42bba3cf4b374
Clément Renaud <clement.renaud@gmail.com>
2017-02-24 18:15:26 +0100

    add 404 page

---
+++ imports/ui/pages/Page404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Page404 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Topogram</h1>
+        <p>Wooops... nothing here !</p>
+      </div>
+    )
+  }
+}
