2228bae15b84cd88672db48e4d44bc1a771faece
Clément Renaud <clement.renaud@gmail.com>
2017-03-03 20:28:54 +0100

    client: add topogram info to the props

--- imports/client/reducers/index.js
+++ imports/client/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
 import { topograms } from './topograms';
+import { topogram } from './topogram';
 import { nodes } from './nodes';
 import { edges } from './edges';
 
 const rootReducer = combineReducers({
   topograms,
+  topogram,
   nodes,
   edges
 });
