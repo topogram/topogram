b5d3ce67612ee0ca753eeaf581da82517026837a
Clément Renaud <clement.renaud@gmail.com>
2017-09-22 13:16:31 +0800

    geoMap switch working

--- imports/ui/components/geoMap/GeoMap.jsx
+++ imports/ui/components/geoMap/GeoMap.jsx
@@ -1,117 +1,117 @@
 import React from 'react'
-import { geoMercator, geoPath } from "d3-geo"
-import { feature } from "topojson-client"
 
-import worldGeoData from './data/110m.json'
+import { LatLng } from 'leaflet';
+import * as d3 from 'd3';
+import { geoTransform, geoMercator, geoPath } from 'd3-geo';
+import { Map, Pane, CircleMarker, TileLayer, Polyline } from 'react-leaflet';
 
-const worldData = feature(worldGeoData, worldGeoData.objects.countries).features
+
+import 'leaflet/dist/leaflet.css'
+import './GeoMap.css'
+
+// import { feature } from 'topojson-client'
+// import worldGeoData from './data/110m.json'
+// const worldData = feature(worldGeoData, worldGeoData.objects.countries).features
 
 const MAP_DIV_ID = "map"
 const style = {
   divMap : {
-    height: '100%',
-    width: '100%',
+    height: '100vh',
+    width: '100vw',
     position: 'fixed',
-    top: '0px',
+    top: '0',
     left: '0',
     zIndex : -1
   }
 }
 
-
 class GeoMap extends React.Component {
 
   constructor(props) {
     super(props)
-
-    this.state = {}
+    this.state = {
+      zoom : 3,
+      position : [51.505, -0.09],
+      w: '100vw',
+      h: '100vh'
+    }
   }
 
-  projection() {
-   return geoMercator()
-     .scale(100)
-     .translate([ 800 / 2, 450 / 2 ])
- }
+  onClickNode() {
+    console.log("click node")
+  }
 
-
+  onClickEdge() {
+    console.log("click edge")
+  }
 
   render() {
+    let {zoom, position, w, h} = this.state
+    let nodesById = {}
 
-    const nodesById = {}
-    const nodes = this.props.nodes.map( (n,i) => {
-      let cx = this.projection()([n.data.lng,n.data.lat])[0],
-        cy = this.projection()([n.data.lng,n.data.lat])[1]
-
-      nodesById[n.data.id] = {...n, cx, cy}
-
-      return (
-        <circle
-          key={i}
-          cx={cx}
-          cy={cy}
-          r={ 5 }
-          fill="#E91E63"
-          className="geoNode"
+    let nodes = this.props.nodes.map( (n,i) => {
+      let coords = [n.data.lat,n.data.lng]
+      nodesById[n.data.id] = coords; // store for edges
+      return <CircleMarker
+        radius={5}
+        key={`node-${i}`}
+        center={coords}
+        onClick={(e) => this.onClickNode(e)}
         />
-      )
     })
 
-
-
-    const edges = this.props.edges.map( (e,i) => {
-
-      let source = nodesById[e.data.source],
-          target = nodesById[e.data.target]
-
-      let x1 = source.cx,
-          y1 = source.cy,
-          x2 = target.cx,
-          y2 = target.cy
-
-      return (
-        <line
-          key={i}
-          x1={x1}
-          x2={x2}
-          y1={y1}
-          y2={y2}
-          stroke={'red'}
-        />
-      )
-    })
+    let edges = this.props.edges.map( (e,i) =>
+      <Polyline
+        key={`edge-${i}`}
+        color="purple"
+        positions={[ nodesById[e.data.source], nodesById[e.data.target] ]}
+        onClick={(e) => this.onClickEdge(e)}
+      />
+    )
 
     return (
-      <div
-        id={MAP_DIV_ID}
-        style={style.divMap}
-      >
-
-        <svg width={ 800 } height={ 450 } viewBox="0 0 800 450">
-          <g className="countries">
-            {
-              worldData.map((d,i) => (
-                <path
-                  key={ `path-${ i }` }
-                  d={ geoPath().projection(this.projection())(d) }
-                  className="country"
-                  fill={ `rgba(38,50,56,${1 / worldData.length * i})` }
-                  stroke="#FFFFFF"
-                  strokeWidth={ 0.5 }
-                />
-              ))
-            }
-          </g>
-          <g className="geoNodes">
-            {nodes}
-          </g>
-          <g className="geoEdges">
-            {edges}
-          </g>
-        </svg>
+      <div style={style.divMap} >
+        <div
+          id={MAP_DIV_ID}
+          style={style.divMap}
+          >
+          <Map
+            center={position}
+            zoom={zoom}
+            ref='map'
+            >
+              {edges}
+              {nodes}
+            <TileLayer
+              url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+          </Map>
+        </div>
       </div>
     )
   }
 }
+
+
+//     <g className="countries">
+//       {
+//         worldData.map((d,i) => (
+//           <path
+//             key={ `path-${ i }` }
+//             d={ geoPath().projection(this.projection())(d) }
+//             className="country"
+//             fill={ `rgba(38,50,56,${1 / worldData.length * i})` }
+//             stroke="#FFFFFF"
+//             strokeWidth={ 0.5 }
+//           />
+//         ))
+//       }
+//     </g>
+//     <g className="geoEdges">
+//       {edges}
+//     </g>
+//   </svg>
 
 GeoMap.propTypes = {
   topogramId : React.PropTypes.string,
