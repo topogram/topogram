33475787043809230b7ffdc3b72ec0eb429fa075
Clément Renaud <clement.renaud@gmail.com>
2017-09-30 10:24:52 +0800

    add App component to routes

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -4,19 +4,26 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import store from '/imports/client/store';
 
-import MainLayout from '/imports/client/ui/layouts/MainLayout';
+
 import { HomeContainer } from '/imports/client/ui/containers/HomeContainer.jsx'
+import App from '/imports/client/ui/containers/App.jsx';
+
 // import { TopogramContainer } from '/imports/client/ui/containers/TopogramContainer.jsx'
 import { TopogramViewContainer } from '/imports/client/ui/containers/TopogramViewContainer.jsx'
+
+import { SignUpPage } from '/imports/client/ui/pages/SignUpPage.jsx'
+import { LoginPage } from '/imports/client/ui/pages/LoginPage.jsx'
 import Page404 from '/imports/client/ui/pages/Page404.jsx'
 
 export const renderRoutes = () => (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={MainLayout}>
+      <Route path="/" component={App}>
         <IndexRoute component={HomeContainer}/>
         {/* <Route path="topograms/:topogramId/edit" component={TopogramContainer}/> */}
         <Route path="topograms/:topogramId" component={TopogramViewContainer}/>
+        <Route path="/signup" component={SignUpPage}/>
+        <Route path="/login" component={LoginPage}/>
         <Route path="*" component={Page404}/>
       </Route>
     </Router>
