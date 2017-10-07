37272383710e8d02ddf7b36dd71905b25794beec
Clément Renaud <clement.renaud@gmail.com>
2017-10-07 14:53:26 +0200

    better description props handling

--- imports/client/ui/components/SidePanel/PanelDescription.jsx
+++ imports/client/ui/components/SidePanel/PanelDescription.jsx
@@ -15,10 +15,14 @@ const PanelDescription = ({
       subtitle={`${nodesCount} nodes, ${edgesCount} edges`}
       />
     <CardText>
-      <Markdown source={topogram.description} />
+      {
+        topogram.description ?
+          <Markdown source={topogram.description} />
+        :
+          null
+      }
 
-      <p>Last modified {moment(topogram.lastModified).fromNow()}</p>
-      <p>Created {moment(topogram.createdAt).fromNow()}</p>
+      <p style={{fontSize:'.9em', color:'#5A5A5A'}}>Last modified {moment(topogram.lastModified).fromNow()} | Created {moment(topogram.createdAt).fromNow()}</p>
     </CardText>
 
   </span>
