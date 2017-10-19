2881fe402a81228e54957129236a5f44ea75602f
ungentilgarçon <gregory.bahde@laposte.net>
2017-10-20 01:09:30 +0200

    adding the button/ no action ye

--- imports/client/ui/components/timeLine/TimeLine.jsx
+++ imports/client/ui/components/timeLine/TimeLine.jsx
@@ -6,6 +6,8 @@ import { Card, CardText, CardHeader } from 'material-ui/Card'
 import DatePicker from 'material-ui/DatePicker'
 import IconButton from 'material-ui/IconButton'
 import PlayCircleFilled from 'material-ui/svg-icons/av/play-circle-filled';
+import TimerMixin from 'react-timer-mixin';
+
 import TimeSlider from './TimeSlider.jsx'
 
 const styleTimeLine = {
@@ -16,6 +18,61 @@ const styleTimeLine = {
 }
 
 @ui()
+//https://stackoverflow.com/questions/40885923/countdown-timer-in-react
+class TimerForTopogram extends React.Component {
+  constructor() {
+    super();
+    this.state = { time: {}, seconds: 5 };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
+
+  secondsToTime(secs){
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    };
+    return obj;
+  }
+
+  componentDidMount() {
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+  }
+
+  startTimer() {
+    if (this.timer == 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    // Remove one second, set state so a re-render happens.
+    let seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
+
+    // Check if we're at zero.
+    if (seconds == 0) {
+      clearInterval(this.timer);
+    }
+  }
+
+}
+
+
 export default class TimeLine extends React.Component {
 
   static propTypes = {
@@ -36,16 +93,27 @@ export default class TimeLine extends React.Component {
 
   openMaxDatePicker = () => {
     this.refs.maxDatePicker.focus()
+    console.log('maxDAte')
   }
 
 
-  playOrPause = (event) => {
-    console.log('maxTime')
-    //for (var i = 0; i < 'maxTime'; i++) {
+  playOrPause = ( ) => {
+    console.log('maxTime', this.props.ui.maxTime)
+    console.log('minTime', this.props.ui.minTime)
+    console.log('ran playOrPause')
 
-    //}
+    var seconds = parseInt((this.props.ui.maxTime-this.props.ui.minTime)/1000);
+    var tempo = Math.floor(seconds/60);
+    var k = 0;
+    while (k < seconds) {
+      this.countDown
+      k+=tempo
+      this.props.updateUI( 'currentSliderTime' , k )
 
+    }
   }
+
+}
 
   render() {
 
@@ -76,7 +144,7 @@ export default class TimeLine extends React.Component {
 
                   <IconButton onClick={this.playOrPause}>
                     <PlayCircleFilled />
-                  </IconButton>     
+                  </IconButton>
                 </p>
               }
             />
