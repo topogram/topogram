6dd6f42ae02b8e83972f3c15cc3a2061102d8a46
Clement Renaud <clement.renaud@gmail.com>
2019-06-14 20:27:48 +0200

    cleanup package.json

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import ReactDOM from 'react-dom';
 // main component
 import Topograph from './Topograph.jsx'
 
-// load data
-import nodes from '../data/arc5/arc5-nodes.json';
-import edges from '../data/arc5/arc5-edges.json';
-import config from '../data/arc5/arc5-topogram.json';
+import csv from 'csvtojson';
 
+// load data
+// import nodes from '../data/arc5/arc5-nodes.json';
+// import edges from '../data/arc5/arc5-edges.json';
+// import config from '../data/arc5/arc5-topogram.json';
+
+csv.fromFile('../data/random/nodes.csv')
+  .then( data => console.log(data))
+
+csv.fromFile('../data/random/edges.csv')
+    .then( data => console.log(data))
 
 // fix dates
 const nodesFix = nodes.map(n => ({
