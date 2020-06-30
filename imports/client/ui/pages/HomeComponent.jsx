b0124cadc85a658a743b7406e0c3fb2152d9ab8a
ungentilgarcon <monsieur.goonie@gmail.com>
2020-06-30 11:20:03 +0200

    added buttons public

--- imports/client/ui/pages/HomeComponent.jsx
+++ imports/client/ui/pages/HomeComponent.jsx
@@ -6,6 +6,7 @@ import { FormattedMessage, defineMessages } from 'react-intl'
 import TopogramsLayout from './TopogramsLayout.jsx'
 
 import TopogramList from '/imports/client/ui/components/topograms/TopogramList.jsx'
+import CardText from 'material-ui/Card'
 
 const messages = defineMessages({
   tagline : {
@@ -100,7 +101,9 @@ export class HomeComponent extends React.Component {
 
           /> */}
 
-
+          <section
+            className="home-maplist"
+          >
         <TopogramList
           topograms={topograms}
           // title={<FormattedMessage {...messages.browseTopograms} />}
@@ -116,6 +119,53 @@ export class HomeComponent extends React.Component {
           autoHideDuration={4000}
           onRequestClose={this.handleRequestClose}
         />
+    </section>
+    <section
+      className="home-explainer"
+    >
+
+
+
+  <p style={{ color: '#aa8dc6',fontSize : '9pt',fontWeight: 'bold', lineHeight : '1em' }}>
+    How to use Bandstour?</p>
+  <p style={{ color: '#aa8dc6',fontSize : '8pt', lineHeight : '1em' }}>
+      SidePanels:</p>
+
+  <p style={{ color: '#F2EFE9',fontSize : '8pt', lineHeight : '1em' }}>
+    Network and Map embed controls allow for finer tuning of the views than mouse controls, try it and you will see for yourself!.</p>
+
+  <p style={{ color: '#F2EFE9',fontSize : '8pt', lineHeight : '1em' }}>
+
+The wheel allows different changes of configuration: Display/Hide legend, Charts, Network, Geo Map,Time. Map background allows changing Geo Map background.
+
+ Network layout allows different network rendering.
+
+ Node radius can be set depending on weight (as provided), or degree of connectivity. Settings Allow different modifications of the graph, including delete (BEWARE).
+ Font Size and DB Settings are quick-and-dirty hacks that allow Network font restyling for optimal viewing and saving, Save Graph Nodes move to DB allows to force saving node moved to DB.
+
+</p>
+<br/>
+<p style={{ color: '#F2EFE9',fontSize : '8pt', lineHeight : '1em' }}>
+
+The Home button allows searching for Nodes (here venues).
+
+</p>
+<p style={{ color: '#aa8dc6',fontSize : '8pt', lineHeight : '1em' }}>
+    Main panels:</p>
+<p style={{ color: '#F2EFE9',fontSize : '8pt', lineHeight : '1em' }}>
+Under Title, Datas shows calculated optimisation of tour.
+Title box also get selected nodes/edges chips. Clicking on them reveals their datas.
+When some are selected, Focu and rearrange redraws a subGraph, ordered, whereas focus only just removes the other nodes/edges from the view.
+
+On timeline, pressing Stop set timedelta to 1 year. Pressing next button iterates 1 year slices. Otherwise play/pause button can allow animation.
+
+Charts has console output stats too, so use the inspector to reveal various Stats if need be.
+</p>
+
+
+
+
+    </section>
       </TopogramsLayout>
     )
   }
